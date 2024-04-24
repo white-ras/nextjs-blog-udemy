058b19b7b4c0870f6969b1f7420653bb67ebaa9e
white-ras <rasras3366@gmail.com>
2024-04-24 15:42:12 +0900

    all done

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import Layout from '../../components/Layout';
+import { getAllPostIds, getPostData } from '../../lib/post';
+import utilStyles from '../../styles/utils.module.css';
+
+//動的ルーティング設定のための関数。pathsがルーティング設定になっている(開発環境なら毎回リクエスト時に実行される、本番環境ならビルド時だけ実行される。)。
+//idがとりうる値のリストを返す
+export async function getStaticPaths() {
+  //ブログ投稿データのファイル名(id)を取得。
+  const paths = getAllPostIds();
+
+  return {
+    paths, //どのパスが事前にレンダリングされるのか決める。
+    fallback: false, //あとで説明。(falseにすると、上のpathsに含まれてないあらゆるパスはアクセスすると404ページになる。)
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}> {postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.blogContentHTML }} />
+      </article>
+    </Layout>
+  );
+}
